ecbdd5b8425bd49858cb10844b0d75f0c90966a4
beatrizabadj <145197915+beatrizabadj@users.noreply.github.com>
2025-01-08 00:04:41 +0100

    Delete static directory

--- static/script.js
+++
@@ -1,8 +0,0 @@
-window.onload = ()=>{
-    let botonSidebar = document.querySelector('.open-btn');
-    let sidebar = document.querySelector('.sidebar-index');
-    //cuando se hace click al boton, la sidebar se muestra
-    botonSidebar.addEventListener('click', () =>{
-        sidebar.classList.toggle('active');
-    });
-};
